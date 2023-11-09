f60eb4fdf660a17ade2b480631890e0ba02766e8
canaydinn <canaydinn@gmail.com>
2023-11-09 15:16:36 +0300

    login router tasarlandı

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db_connect=require('./db/mysql_connect')
+const router=require('./routers')
 require('dotenv/config')
 const app = express()
 app.get('/', function (req, res) {
@@ -8,5 +9,7 @@ app.get('/', function (req, res) {
 app.get('/app', function (req, res) {
     res.send('app sayfası')
   })
-
+//Midllewares
+app.use(express.json({limit:'50mb',extended:true,parameterLimit:50000}))
+app.use('/api',router)
 app.listen(process.env.PORT)
